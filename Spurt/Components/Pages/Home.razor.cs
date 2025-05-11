eafd0d768ae11f0aec7ca4c1b56a919b003ef82a
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-11 17:59:24 +0200

    If user is registered, show welcome message. If not, redirect to registration

---
+++ Spurt/Components/Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Spurt.Data.Queries;
+using Spurt.Domain.Player;
+
+namespace Spurt.Components.Pages;
+
+public partial class Home(
+    ILocalStorageService localStorageService,
+    NavigationManager navigationManager,
+    IGetPlayer getPlayer)
+{
+    private Player? CurrentPlayer { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+
+        var playerId = await localStorageService.GetItemAsync<Guid?>("PlayerId");
+        if (playerId == null)
+        {
+            navigationManager.NavigateTo("/registerplayer");
+            return;
+        }
+
+        CurrentPlayer = await getPlayer.Execute(playerId.Value);
+        if (CurrentPlayer == null) navigationManager.NavigateTo("/registerplayer");
+    }
+}
