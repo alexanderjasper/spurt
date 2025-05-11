bfac953311d9c027d1852140e1104eeb649c01d1
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-11 20:27:34 +0200

    Add migration for Games table

---
+++ Spurt/Migrations/20250511182723_AddGames.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spurt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", maxLength: 6, nullable: false),
+                    CreatorId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GamePlayer",
+                columns: table => new
+                {
+                    GameId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PlayersId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlayer", x => new { x.GameId, x.PlayersId });
+                    table.ForeignKey(
+                        name: "FK_GamePlayer_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlayer_Players_PlayersId",
+                        column: x => x.PlayersId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayer_PlayersId",
+                table: "GamePlayer",
+                column: "PlayersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_CreatorId",
+                table: "Games",
+                column: "CreatorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePlayer");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
