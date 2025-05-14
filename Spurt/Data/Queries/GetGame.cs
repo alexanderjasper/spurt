c0a5be7ee0b5dc85c34ca99cd43dbe0222725ab1
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-14 20:03:52 +0200

    Add query to get game

---
+++ Spurt/Data/Queries/GetGame.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Spurt.Domain.Games;
+
+namespace Spurt.Data.Queries;
+
+public class GetGame(AppDbContext dbContext) : IGetGame
+{
+    public async Task<Game?> Execute(string code)
+    {
+        return await dbContext.Games
+            .Include(g => g.Players)
+            .Include(g => g.Creator)
+            .FirstOrDefaultAsync(g => g.Code == code);
+    }
+}
+
+public interface IGetGame
+{
+    Task<Game?> Execute(string code);
+}
