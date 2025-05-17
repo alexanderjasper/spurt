8fd5fc60a581c7d14add57e691e5afacb62da2e6
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-17 09:42:20 +0200

    Fix issue where gamehub would fail

--- Spurt/Program.cs
+++ Spurt/Program.cs
@@ -30,7 +30,7 @@ builder.Services.Scan(scan => scan
 );
 
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<IGameHubConnectionService, GameHubConnectionService>();
+builder.Services.AddTransient<IGameHubConnectionService, GameHubConnectionService>();
 builder.Services.AddScoped<IGameHubNotificationService, GameHubNotificationService>();
 
 var app = builder.Build();
