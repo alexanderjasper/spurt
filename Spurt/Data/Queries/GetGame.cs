39a521ce5917ffec2ed856b2ea9b914402ad0fc0
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-17 16:32:24 +0200

    Fix null reference issue

--- Spurt/Data/Queries/GetGame.cs
+++ Spurt/Data/Queries/GetGame.cs
@@ -14,6 +14,7 @@ public class GetGame(AppDbContext dbContext) : IGetGame
             .ThenInclude(p => p.Category)
             .ThenInclude(c => c!.Clues)
             .Include(g => g.SelectedClue)
+            .ThenInclude(c => c!.Category)
             .Include(g => g.BuzzedPlayer)
             .AsQueryable();
 
