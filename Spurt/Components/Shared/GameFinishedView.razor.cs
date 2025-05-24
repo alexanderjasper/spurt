0c0345f4f4a199450485762bb732679cd7b69ed7
Alexander Skjold Jasper <alexander.jasper@visma.com>
2025-05-24 21:42:18 +0200

    Add button to return to home page

--- Spurt/Components/Shared/GameFinishedView.razor.cs
+++ Spurt/Components/Shared/GameFinishedView.razor.cs
@@ -5,9 +5,11 @@ using Spurt.Domain.Players;
 
 namespace Spurt.Components.Shared;
 
-public partial class GameFinishedView
+public partial class GameFinishedView(NavigationManager NavigationManager)
 {
     [Parameter] [Required] public required Game Game { get; set; }
 
     [Parameter] [Required] public Player? CurrentPlayer { get; set; }
+    
+    private void GoToHome() => NavigationManager.NavigateTo("/");
 }
